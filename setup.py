c524f7f7c2572130e431ba5814be62817783e05d
Jason Tolbert <jasonalantolbert@gmail.com>
2021-01-25 16:54:25 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-orm',
+    version='1.0.0',
+    packages=['src', 'src.django_orm'],
+    url='https://github.com/jasonalantolbert/django-orm',
+    license='MIT',
+    author='Jason Tolbert',
+    author_email='jasonalantolbert@gmail.com',
+    description='A Python library that enables you to harness the power of Django\'s ORM in standalone Python scripts.'
+)

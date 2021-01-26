46c4022b9be7ea21ca3ce50dd8cd733c25f19571
Jason Tolbert <jasonalantolbert@gmail.com>
2021-01-26 15:39:24 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='standalorm',
+    version='1.0.0',
+    packages=['src', 'src.standalorm'],
+    url='https://github.com/jasonalantolbert/standalorm',
+    license='MIT',
+    author='Jason Tolbert',
+    author_email='jasonalantolbert@gmail.com',
+    description='A Python library that enables you to harness the power of Django\'s ORM in standalone Python scripts.'
+)

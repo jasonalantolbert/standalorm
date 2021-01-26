4bdc01d2ef6009be566e633319342db9d6c32713
Jason Tolbert <jasonalantolbert@gmail.com>
2021-01-26 15:52:19 -0500

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', 'src', 'standalorm')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'src')))
 
 # noinspection PyUnresolvedReferences
 import sphinx_rtd_theme

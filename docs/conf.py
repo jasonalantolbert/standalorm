47e25af7fffaef905c5eb385bdb9fcb22f4bec06
Jason Tolbert <jasonalantolbert@gmail.com>
2021-01-25 23:47:28 -0500

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,11 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-
-# noinspection PyUnresolvedReferences
-sys.path.insert(0, os.path.abspath(os.path.join('..', 'src')))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath(os.path.join('..', 'src')))
 
 # noinspection PyUnresolvedReferences
 import sphinx_rtd_theme
